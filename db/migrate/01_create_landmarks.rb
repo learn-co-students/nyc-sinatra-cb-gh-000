f97325ecfd0bbf28465b5f11091b32b8ff8d5694
Mondo Gao <mondogao@gmail.com>
2017-02-07 13:48:42 +0000

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,22 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+  def change
+    create_table :figures do |t|
+      t.string :name
+    end
+
+    create_table :landmarks do |t|
+      t.string :name
+      t.belongs_to :figure, index: true
+      t.integer :year_completed
+    end
+
+    create_table :titles do |t|
+      t.string :name
+    end
+
+    create_table :figure_titles do |t|
+      t.belongs_to :figure, index: true
+      t.belongs_to :title, index: true
+    end
+  end
 end
