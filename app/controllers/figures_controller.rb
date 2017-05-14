d2681cb9d634ee64e22e89361a63bea9a72628a9
Robert Wayne <robertjwayne@gmail.com>
2017-05-14 19:15:01 -0400

    Add index, show, new, and edit views to routes

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,18 +2,22 @@ class FiguresController < ApplicationController
   set :views, (Proc.new { File.join(root, "../views/figures/") })
 
   get '/figures' do
+    erb :index
   end
 
   post '/figures' do
   end
 
   get '/figures/new' do
+    erb :new
   end
 
   get '/figures/:id/edit' do
+    erb :edit
   end
 
   get '/figures/:id' do
+    erb :show
   end
 
   patch '/figures/:id' do
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,18 +2,22 @@ class LandmarksController < ApplicationController
   set :views, (Proc.new { File.join(root, "../views/landmarks/") })
 
   get '/landmarks' do
+    erb :index
   end
 
   post '/landmarks' do
   end
 
   get '/landmarks/new' do
+    erb :new
   end
 
   get '/landmarks/:id/edit' do
+    erb :edit
   end
 
   get '/landmarks/:id' do
+    erb :show
   end
 
   patch '/landmarks/:id' do
