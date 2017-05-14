f440d986dac8ad95618aa079e6c9019f8d9e81eb
Robert Wayne <robertjwayne@gmail.com>
2017-05-14 19:09:03 -0400

    Add routes to LandmarksController

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,21 @@
 class LandmarksController < ApplicationController
+  set :views, (Proc.new { File.join(root, "../views/landmarks/") })
 
+  get '/landmarks' do
+  end
+
+  post '/landmarks' do
+  end
+
+  get '/landmarks/new' do
+  end
+
+  get '/landmarks/:id/edit' do
+  end
+
+  get '/landmarks/:id' do
+  end
+
+  patch '/landmarks/:id' do
+  end
 end
