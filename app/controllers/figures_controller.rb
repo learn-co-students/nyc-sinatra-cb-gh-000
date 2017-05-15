66365ae8a1abd7ba26ee3954ec71b4c021d23514
Robert Wayne <robertjwayne@gmail.com>
2017-05-14 23:21:02 -0400

    Clean up

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,5 @@
 class FiguresController < ApplicationController
-  set :views, (Proc.new { File.join(root, "../views/figures/") })
+  set :views, (Proc.new {File.join(root, "../views/figures/")})
 
   get '/figures' do
     @figures = Figure.all
