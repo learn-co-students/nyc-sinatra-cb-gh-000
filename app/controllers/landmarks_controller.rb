ece7d690aaaf8d086e7a208dfa45972782fc9748
Robert Wayne <robertjwayne@gmail.com>
2017-05-14 23:02:09 -0400

    Build index page for landmarks

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -2,13 +2,19 @@ class LandmarksController < ApplicationController
   set :views, (Proc.new { File.join(root, "../views/landmarks/") })
 
   get '/landmarks' do
+    @landmarks = Landmark.all
     erb :index
   end
 
   post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to("/landmarks/#{@landmark.id}")
   end
 
   get '/landmarks/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :new
   end
 
