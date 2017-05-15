e39a90613b65a9337f2d11f25de66e832b633ef7
Robert Wayne <robertjwayne@gmail.com>
2017-05-14 20:36:32 -0400

    Build form to create a new figure

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,9 +6,25 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    unless params[:title][:name].empty?
+      @title = Title.find_or_create_by(name: params[:title][:name])
+      @figure.titles << @title
+    end
+
+    unless params[:landmark][:name].empty?
+      @landmark = Landmark.find_or_create_by(name: params[:landmark][:name])
+      @figure.landmarks << @landmark
+    end
+
+    redirect to("/figures/#{@figure.id}")
   end
 
   get '/figures/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :new
   end
 
