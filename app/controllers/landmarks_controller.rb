9605257d0f8a7f670578b6f2437aed4af976a7b4
Othman Mashaab <red.fantastic@hotmail.com>
2018-02-16 15:21:57 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,88 @@
 class LandmarksController < ApplicationController
+  get '/' do
+    "HELLOO"
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"/landmarks/new"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"/landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :"/landmarks/edit"
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    @landmark.save
+    redirect to '/landmarks'
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 
 end
--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,8 @@
-class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+class CreateLandmarks < ActiveRecord::Migration[4.2]
+  create_table :landmarks do |t|
+    t.string :name
+    t.integer :year_completed
+    t.integer :figure_id
+    t.timestamps
+  end
 end
